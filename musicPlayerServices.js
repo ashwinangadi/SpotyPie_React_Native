21c56a681f207149916b5cc6b74fa8683473f359
Ashwin <ashwin.angadi1@gmail.com>
2024-01-19 13:24:27 +0530

    player logic update.

--- musicPlayerServices.js
+++ musicPlayerServices.js
@@ -1,6 +1,25 @@
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, {RepeatMode} from 'react-native-track-player';
 
 import {playListData} from './src/constants';
+
+export async function setupPlayer() {
+  let isSetup = false;
+  try {
+    await TrackPlayer.getActiveTrackIndex();
+    isSetup = true;
+  } catch (error) {
+    await TrackPlayer.setupPlayer();
+    isSetup = true;
+  } finally {
+    return isSetup;
+  }
+}
+
+// To load tracks to the player
+export async function addTrack() {
+  await TrackPlayer.add(playListData); // This adds track data to the player
+  await TrackPlayer.setRepeatMode(RepeatMode.Queue); // This can be used to set repeat button in the player ui
+}
 
 export async function playbackService() {
   TrackPlayer.addEventListener(Event.RemotePause, () => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
-import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {addTrack, setupPlayer} from '../musicPlayerServices';
 
 function App(): React.JSX.Element {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  async function setup() {
+    const isReady = await setupPlayer();
+    if (isReady) {
+      await addTrack();
+    }
+    setIsPlayerReady(isReady);
+  }
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
